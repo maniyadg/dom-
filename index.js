824a43048177c2e4c8887869733755465e62dcba
Maniyarasan <92170430+maniyadg@users.noreply.github.com>
2022-09-01 14:57:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function foo(){
+    var result=document.getElementById("main").value;
+    console.log(result)
+    var result1=document.getElementById("main1").value;
+    console.log(result1)
+    var result2=document.getElementById("main2").value;
+    console.log(result2)
+    var result3=document.getElementById("main3").value;
+    console.log(result3)
+}
